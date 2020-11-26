ad220ac5da658b58c8d2c0286c386c3da2cf5603
Luis uribe lozano <luisuribelozano@gmail.com>
2020-11-25 22:11:12 -0500

    Avance comunicacion entre componente hijo a padre y padre a hijo

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,29 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styles: []
 })
-export class IncrementadorComponent {
-
-  progress: number = 40;  
-   
-  get getPorcentaje(){
-    return `${ this.progress }%`;
+export class IncrementadorComponent implements OnInit {
+  ngOnInit() {
+    this.btnClass = `btn ${ this.btnClass }`;
   }
-   
 
+  // de esta manera se renombra el argumento
+  // @Input('valor') progress: number = 40;
+  @Input() progress: number = 40; 
+  @Input() btnClass: string = 'btn-primary';
+
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter();
+   
   cambiarValor(valor: number) {
 
-    if(this.progress >= 100 && valor >= 0){
-       return this.progress = 100;      
+    if(this.progress >= 100 && valor >= 0) {
+        this.valorSalida.emit(100);
+        return this.progress = 100;      
     }
 
-    if(this.progress <= 0 && valor < 0 ){
+    if(this.progress <= 0 && valor < 0 ) {
+      this.valorSalida.emit(100);
       return this.progress = 0;
     }
     this.progress = this.progress + valor;
+    this.valorSalida.emit( this.progress );
+  }
+
+  onChange(nuevoValor: number) { 
+
+    if( nuevoValor >= 100 ){
+      this.progress = 100;
+    } else if ( nuevoValor <= 0) {
+      this.progress = 0;
+    } else { 
+      this.progress = nuevoValor;
+    }
+    this.valorSalida.emit( this.progress );
   }
 
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -6,5 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent {
-   
+   progress1: number = 25;
+   progress2: number = 35;
+
+   get getProgress1() {
+     return `${ this.progress1 }%`;
+   }
+
+   get getProgress2() {
+    return `${ this.progress2 }%`;
+  }   
 }
